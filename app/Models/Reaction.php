31cd3396c408a08296028d63e57b5d6179201196
unknown <victor.ibarra.olivares@gmail.com>
2021-04-14 09:39:23 -0400

    primer commit

---
+++ app/Models/Reaction.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reaction extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+    
+    const LIKE = 1;
+    const DISLIKE = 2;
+
+    // Relacion uno a muchos inversa
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function reactionable()
+    {
+        return $this->morphTo();
+    }
+}
