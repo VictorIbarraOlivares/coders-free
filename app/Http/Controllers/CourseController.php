04d9c7d06c7c99616f72c6c5fcc79264cc107bd1
unknown <victor.ibarra.olivares@gmail.com>
2021-04-21 09:08:54 -0400

    se termina la vista show de los cursos

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -14,6 +14,13 @@ class CourseController extends Controller
 
     public function show(Course $course)
     {
-        return view('courses.show', compact('course'));
+        $similares = Course::where('category_id', $course->category_id)
+        ->where('id', '!=', $course->id)
+        ->where('status', 3)
+        ->latest('id')
+        ->take(5)
+        ->get();
+
+        return view('courses.show', compact('course', 'similares'));
     }
 }
--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -14,13 +14,13 @@
             </div>
         </div>
     </section>
-    <div class="container grid grid-cols-3">
-        <div class="col-span-2">
+    <div class="container grid grid-cols-1 lg:grid-cols-3  gap-6">
+        <div class="order-2 lg:col-span-2 lg:order-1">
             <section class="card mb-12">
                 <div class="card-body">
-                    <h1 class="font-bold text-2xl mb-2">Lo que arpenderás</h1>
+                    <h1 class="font-bold text-2xl mb-2">Lo que aprenderás</h1>
 
-                    <ul class="grid grid-cols-2 gap-x-6 gap-y-2">
+                    <ul class="grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-2">
                         @foreach ($course->goals as $goal)
                             <li class="text-gray-700 text-base">
                                 <i class="fas fa-check text-gray-600 mr-2"></i>{{ $goal->name }}</li>
@@ -33,25 +33,82 @@
                 <h1 class="font-bold text-3xl mb-2">Temario</h1>
 
                 @foreach ($course->sections as $section)
-                    <article class="mb-4 shadow">
-                        <header class="border border-gray-200 px-4 py-2 cursor-pointer bg-gray-200">
+                    <article 
+                        @if ( $loop->first )
+                            x-data="{open : true}" 
+                        @else
+                            x-data="{open : false}"
+                        @endif
+                        class="mb-4 shadow">
+                        <header x-on:click="open = !open" class="border border-gray-200 px-4 py-2 cursor-pointer bg-gray-200">
                             <h1 class="font-bold text-lg text-gray-600">{{ $section->name }}</h1>
                         </header>
 
-                        <div class="bg-white py-2 px-4">
-                            <ul>
+                        <div x-show="open" class="bg-white py-2 px-4">
+                            <ul class="grid grid-cols-1 gap-2">
                                 @foreach ($section->lessons as $lesson)
-                                    <li>{{ $lesson->name }}</li>
+                                    <li class="text-gray-700 text-base"><i class="fas fa-play-circle mr-2 text-gray-600"></i>{{ $lesson->name }}</li>
                                 @endforeach
                             </ul>
                         </div>
                     </article>
                 @endforeach
             </section>
+
+            <section class="mb-8">
+                <h1 class="font-bold text-3xl">Requisitos</h1>
+
+                <ul class="list-disc list-inside">
+                    @foreach ($course->requirements as $requirement)
+                        <li class="text-gray-700 text-base" >{{ $requirement->name }}</li>
+                    @endforeach
+                </ul>
+            </section>
+
+            <section >
+                <h1 class="font-bold text-3xl">Descripción</h1>
+
+                <div class="text-gray-700 text-base">
+                    {{ $course->description }}
+                </div>
+            </section>
         </div>
 
-        <div>
-            
+        <div class="order-1 lg:order-2">
+            <section class="card mb-4">
+                <div class="card-body">
+                    <div class="flex items-center">
+                        <img class="h-12 w-12 object-cover rounded-full shadow-lg" src="{{ $course->teacher->profile_photo_url }}" alt="{{ $course->teacher->name }}">
+                        <div class="ml-4">
+                            <h1 class="font-bold text-gray-500 text-lg">Prof. {{ $course->teacher->name }}</h1>
+                            <a class="text-blue-400 text-sm font-bold" href="">{{'@'.Str::slug($course->teacher->name, '') }}</a>
+                        </div>
+                    </div>
+                    <a class="btn btn-danger btn-block mt-4">Llevar este curso</a>
+                </div>
+            </section>
+
+            <aside class="hidden lg:block">
+                @foreach ($similares as $similar)
+                    <article class="flex mb-6">
+                        <img class="h-32 w-40 object-cover" src="{{ Storage::url($similar->image->url) }}">
+                        <div class="ml-3">
+                            <h1>
+                                <a class="font-bold text-gray-500 mb-3" href="{{ route('courses.show', $similar) }}">{{ Str::limit($similar->title, 40, '...') }}</a>
+                            </h1>
+
+                            <div class="flex items-center mb-2">
+                                <img class="h-8 w-8 object-cover rounded-full shadow-lg" src="{{ $similar->teacher->profile_photo_url }}" alt="{{ $similar->teacher->name }}">
+                                <p class="text-gray-700 text-sm ml-2">{{ $similar->teacher->name }}</p>
+                            </div>
+
+                            <p class="text-sm">
+                                <i class="fas fa-star mr-2 text-yellow-400"></i>{{ $similar->rating }}
+                            </p>
+                        </div>
+                    </article>
+                @endforeach
+            </aside>
         </div>
     </div>
 </x-app-layout>
