e3a722b94fb1c89f0ddeced93cb0523c6dd1bb41
unknown <victor.ibarra.olivares@gmail.com>
2021-05-02 15:04:36 -0400

    se muestra listado de cursos de instructor

--- app/Http/Livewire/InstructorCourses.php
+++ app/Http/Livewire/InstructorCourses.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Course;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class InstructorCourses extends Component
 {
+    use WithPagination;
+    public $search = '';
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+    
     public function render()
     {
-        return view('livewire.instructor-courses');
+        $courses = Course::where('title', 'LIKE', "%{$this->search}%")
+        ->where('user_id', auth()->user()->id )->paginate(8);
+        return view('livewire.instructor-courses', ['courses' => $courses]);
     }
 }
--- resources/views/livewire/instructor-courses.blade.php
+++ resources/views/livewire/instructor-courses.blade.php
@@ -1,3 +1,119 @@
-<div>
-    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+<div class="container py-8">
+    <x-table-responsive>
+
+        <div class="px-6 py-4">
+            <input wire:model='search' class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-blue-400" placeholder="Ingrese el nombre de un curso ...">
+        </div>
+
+        <table class="min-w-full divide-y divide-gray-200">
+            <thead class="bg-gray-50">
+                <tr>
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Nombre
+                    </th>
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Matriculados
+                    </th>
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Calificación
+                    </th>
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Status
+                    </th>
+                    <th scope="col" class="relative px-6 py-3">
+                        <span class="sr-only">Edit</span>
+                    </th>
+                </tr>
+            </thead>
+            <tbody class="bg-white divide-y divide-gray-200">
+                @forelse ($courses as $course)
+                    <tr>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            <div class="flex items-center">
+                                <div class="flex-shrink-0 h-10 w-10">
+                                    <img class="h-10 w-10 rounded-full" src="{{ Storage::url($course->image->url) }}" alt="">
+                                </div>
+                                <div class="ml-4">
+                                    <div class="text-sm font-medium text-gray-900">
+                                        {{ $course->title }}
+                                    </div>
+                                    <div class="text-sm text-gray-500">
+                                        {{ $course->category->name }}
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            <div class="text-sm text-gray-900">{{ $course->students_count }}</div>
+                            <div class="text-sm text-gray-500">Alumnos matriculados</div>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            <div class="text-sm text-gray-900 flex items-center">
+                                {{ $course->rating }}
+                                <ul class="flex text-sm ml-2">
+                                    <li class="mr-1">
+                                        <i class="fas fa-star text-{{ $course->rating >= 1 ? 'yellow' : 'gray' }}-400"></i>
+                                    </li>
+                    
+                                    <li class="mr-1">
+                                        <i class="fas fa-star text-{{ $course->rating >= 2 ? 'yellow' : 'gray' }}-400"></i>
+                                    </li>
+                    
+                                    <li class="mr-1">
+                                        <i class="fas fa-star text-{{ $course->rating >= 3 ? 'yellow' : 'gray' }}-400"></i>
+                                    </li>
+                    
+                                    <li class="mr-1">
+                                        <i class="fas fa-star text-{{ $course->rating >= 4 ? 'yellow' : 'gray' }}-400"></i>
+                                    </li>
+                    
+                                    <li class="mr-1">
+                                        <i class="fas fa-star text-{{ $course->rating == 5 ? 'yellow' : 'gray' }}-400"></i>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="text-sm text-gray-500">Valoración del curso</div>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            @switch($course->status)
+                                @case(1)
+                                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
+                                        Borrador
+                                    </span>     
+                                    @break
+                                @case(2)
+                                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">
+                                        Revisión
+                                    </span>
+                                    @break
+                                @case(3)
+                                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                        Publicado
+                                    </span>
+                                    @break
+                                @default
+                                    
+                            @endswitch
+                            
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                            <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                        </td>
+                    </tr>  
+                @empty
+                    <tr>
+                        <th colspan="5" scope="col" class="relative px-6 py-3 items-center">
+                            <span>No hay registros que coincidan con la búsqueda "{{ $search }}"</span>
+                        </th>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+
+        <div class="px-6 py-4">
+            {{ $courses->links() }}
+        </div>
+
+    </x-table-responsive>
 </div>
