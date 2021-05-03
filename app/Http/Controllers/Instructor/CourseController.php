2ddc29aa75c5e5492eef7282c6ee10b3f374e86b
unknown <victor.ibarra.olivares@gmail.com>
2021-05-03 09:55:41 -0400

    mas avances en el formulario para actualizar información básica de los cursos

--- app/Http/Controllers/Instructor/CourseController.php
+++ app/Http/Controllers/Instructor/CourseController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Instructor;
 use App\Models\Course;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Level;
+use App\Models\Price;
 
 class CourseController extends Controller
 {
@@ -64,7 +67,10 @@ class CourseController extends Controller
      */
     public function edit(Course $course)
     {
-        return view('instructor.courses.edit', compact('course'));
+        $categories = Category::pluck('name' ,'id');
+        $levels = Level::pluck('name', 'id');
+        $prices = Price::pluck('name', 'id');
+        return view('instructor.courses.edit', compact('course', 'categories', 'levels', 'prices'));
     }
 
     /**
