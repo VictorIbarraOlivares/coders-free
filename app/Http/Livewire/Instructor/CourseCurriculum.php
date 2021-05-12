3bef95afdd13abeb8d146d468707e460a789d0d0
unknown <victor.ibarra.olivares@gmail.com>
2021-05-12 18:06:52 -0400

    se crea nueva plantilla para los instructores, ademas de una directiva blade

---
+++ app/Http/Livewire/Instructor/CourseCurriculum.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Livewire\Instructor;
+
+use App\Models\Course;
+use Livewire\Component;
+
+class CourseCurriculum extends Component
+{
+    public $course;
+
+    public function mount(Course $course)
+    {
+        $this->course = $course;
+    }
+    
+    public function render()
+    {
+        return view('livewire.instructor.course-curriculum')->layout('layouts.instructor');
+    }
+}
--- routes/instructor.php
+++ routes/instructor.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Instructor\CourseController;
+use App\Http\Livewire\Instructor\CourseCurriculum;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::redirect('', 'instructor/courses');
 Route::resource('courses', CourseController::class)->names('courses');
+Route::get('courses/{course}/curriculum', CourseCurriculum::class)->name('courses.curriculum');
