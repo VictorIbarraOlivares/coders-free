634cf886903d2c95b2805faa97d7d94cb79618bc
unknown <victor.ibarra.olivares@gmail.com>
2021-05-21 15:43:19 -0400

    se muestran los estudiantes del curso al editar

--- routes/instructor.php
+++ routes/instructor.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Instructor\CourseController;
 use App\Http\Livewire\Instructor\CourseCurriculum;
+use App\Http\Livewire\Instructor\CourseStudents;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +20,4 @@ Route::redirect('', 'instructor/courses');
 Route::resource('courses', CourseController::class)->names('courses');
 Route::get('courses/{course}/curriculum', CourseCurriculum::class)->name('courses.curriculum');
 Route::get('courses/{course}/goals', [CourseController::class, 'goals'])->name('courses.goals');
+Route::get('courses/{course}/students', CourseStudents::class)->name('courses.students');
