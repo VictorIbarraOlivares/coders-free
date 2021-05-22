6a80527a976008af68b20b8b298f83c6ff798ff3
unknown <victor.ibarra.olivares@gmail.com>
2021-05-22 15:41:11 -0400

    se envia email al aprobar curso

--- app/Http/Controllers/Admin/CourseController.php
+++ app/Http/Controllers/Admin/CourseController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Course;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ApprovedCourse;
 
 class CourseController extends Controller
 {
@@ -27,6 +29,10 @@ class CourseController extends Controller
         }
         $course->status = Course::PUBLICADO;
         $course->save();
+        // Enviar correo electronico
+        $mail = new ApprovedCourse($course);
+        Mail::to($course->teacher->email)->send($mail);
+        
         return redirect()->route('admin.courses.index')->with('info', 'El curso se publicó con éxito');
     }
 }
