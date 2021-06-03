6c73e4226db0776b11eb64e3326a03b68b67b7f6
unknown <victor.ibarra.olivares@gmail.com>
2021-06-03 17:50:30 -0400

    se crea crud de categorias

--- routes/admin.php
+++ routes/admin.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\Admin\CourseController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\CourseController;
+use App\Http\Controllers\Admin\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +22,7 @@ Route::get('', [HomeController::class, 'index'])->middleware('can:Ver dashboard'
 
 Route::resource('roles', RoleController::class)->names('roles');
 Route::resource('users', UserController::class)->only(['index', 'edit', 'update'])->names('users');
+Route::resource('categories', CategoryController::class)->names('categories');
 Route::get('courses', [CourseController::class, 'index'])->name('courses.index');
 Route::get('courses/{course}', [CourseController::class, 'show'])->name('courses.show');
 Route::post('courses/{course}/approved', [CourseController::class, 'approved'])->name('courses.approved');
